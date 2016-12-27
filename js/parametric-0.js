c86e4ce205bbfbeb4f774c46e160c4fbef46dcc0
Tianran Chen <ti@nranchen.org>
2016-12-26 23:14:01 -0600

    First parametric curve

--- js/parametric-0.js
+++ js/parametric-0.js
@@ -1,10 +1,28 @@
-var x = 0;
+var t = 0.0;
+var dt = 0.02;
+var r, x, y;
+
+function update() {
+  r = 0.8*min(width/2,height/2);
+  x = r*cos(t);
+  y = r*sin(t);
+}
 
 function setup() {
+  createCanvas(500,500);
+  background(255,255,255);
   frameRate(30);
 }
 
 function draw() {
-  x = x + 1;
-  ellipse(x,50,20,20);
+  update();
+
+  var alpha = trace_slider.value();
+  background(255,255,255,5);
+  ellipseMode(CENTER);
+  noStroke();
+  fill(255,0,0);
+  ellipse(width/2 + x,height/2-y,10,10);
+
+  t = t + dt;
 }
