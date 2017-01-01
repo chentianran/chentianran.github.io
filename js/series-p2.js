785c003cf6e64446824649e9e7f3fc582b386b7d
Tianran Chen <ti@nranchen.org>
2017-01-01 17:52:28 -0600

    Added the p-2 series

---
+++ js/series-p2.js
@@ -0,0 +1,56 @@
+var k = 0;
+var sum = 0.0;
+var step = 40;
+var tw = 40;
+
+function denom(n) {
+  return n*n;
+}
+
+function setup() {
+  var canvas = createCanvas(600,200);
+  canvas.parent('sketch-holder');
+  frameRate(60);
+}
+
+function draw() {
+
+  var n = Math.floor(k / step) + 1;
+  var r = k % step;
+  var d = denom(n);
+
+  if (0 == r) {
+    sum = sum + (1.0 / d);
+  }
+
+  background("#f2f2f2");
+  fill(0,0,0);
+  text("n = ", width/2, height/2);
+  text(n, width/2+20, height/2);
+
+  text("S = ", width/2, height/2+20);
+  text(sum, width/2+20, height/2 + 20);
+
+  var i;
+  for (i = -5; i < 12; i++) {
+    var dx = tw * (i + r/step);
+    var tx = width*0.75 - dx;
+    if (i < 0) {
+      fill(0,0,0,80 + i*25);
+    } else {
+      fill(0,0,0,255 - i*20);
+    }
+    if (n-i > 0) {
+      text("1",tx,15);
+      text("─",tx,22);
+      text("+",tx-10,22);
+      text(denom(n-i),tx,30);
+    }
+  }
+  k = k + 1;
+
+  if ((0 == r) && (step > 5)) {
+    step = step - 1;
+    k = step * (n-1) + 1;
+  }
+}
---
+++ teaching/series-p2.markdown
@@ -0,0 +1,22 @@
+---
+layout: default
+title: Harmonic series
+---
+
+# Hyperharmonic series (P=2)
+
+A hyperharmonic series (a.k.a. P-series) is a generalization of the
+[harmonic series](series-p1/) that has the form
+$$\sum_{n=1}^{\infty} \frac{1}{n^p}$$ for some fixed real number $$p$$.
+If you plug in $$p=1$$, you get exactly the harmonic series.
+The demo below shows how the partial sum $$S$$ of the hyperharmonic series
+$$\sum_{n=1}^{\infty} \frac{1}{n^2}$$ grows as $$n \to \infty$$.
+Can you tell if it converges to a number?
+What will that number be?
+
+<div id="sketch-holder">
+  <!-- Our sketch will go here! -->
+</div>
+
+<script language="javascript" type="text/javascript" src="/js/p5.min.js"></script>
+<script language="javascript" type="text/javascript" src="/js/series-p2.js"></script>
